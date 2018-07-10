5ff2edcc6a99d3ef0bbb680210c96a6d8a15dd77
YosriMimouna <yosri.mimouna@ensi-uma.tn>
2018-07-10 23:28:57 +0100

    defined methods for game class

--- Chess/game.h
+++ Chess/game.h
@@ -2,20 +2,29 @@
 #define GAME_H
 #include "piece.h"
 #include "player.h"
+#include "constants.h"
 
 class Game
 {
-    Piece* Board[8][8];
-    Piece* WhitePieces[16];
-    Piece* BlackPieces[16];
-    Piece* WhitePawned[15];
-    Piece* BlackPawned[15];
-    Player P1,P2;
-    int Time1,Time2;
-public:
+    Color turn;
+    Piece* board[8][8];
+    bool mate;
+    Piece* whitePieces[16];
+    Piece* blackPieces[16];
+    Piece* whitePawned[15];
+    Piece* blackPawned[15];
+    Player p1,p2;
+    int time0,time1,time2;
 
 public:
     Game();
+    ~Game();
+    Piece* getSquare(int x,int y);
+    void setSquare(Piece* p,int x,int y);
+    void generatePossibleMoves();
+    void assist(int x,int y);
+    void play(int x1,int y1,int x2,int y2);
+    void capture(int x,int y);
 };
 
 #endif // GAME_H

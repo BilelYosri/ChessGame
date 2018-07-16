f519b7a0a379664ade7384dcc9eedbc7b95a9065
YosriMimouna <yosri.mimouna@ensi-uma.tn>
2018-07-16 23:18:45 +0100

    Implemented generatePossibilities() in rook.cpp

--- Chess/rook.cpp
+++ Chess/rook.cpp
@@ -10,7 +10,43 @@ Rook::Rook(Game* _game, Color _color, int _x, int _y):Piece(_game, _color, _x, _
 bool Rook::generatePossibilities()
 {
     memset(possibilities,false,sizeof (possibilities));
-
+    int xi = x;
+    int yi = y;
+    int xp,yp;
+    bool exists = false;
+    bool canMove;
+    Piece* p;
+    King* k = game->getKing();
+    for(int i=0;i<4;i++)
+    {
+        xp = xi;
+        yp = yi;
+        do
+        {
+            canMove=false;
+            xp = xp + ROOK_CASTLE_X[i];
+            yp = yp + ROOK_CASTLE_Y[i];
+            p = game->getSquare(xp,yp);
+            if (p == nullptr && !(game->isOutOfBoundaries(xp,yp)))
+            {
+                canMove=true;
+                game->setSquare(this,xp,yp);
+                if (!(k->isInCheck()))
+                    exists = possibilities[xp][yp] = true;
+                game->setSquare(this,xi,yi);
+            }
+        }
+        while( canMove );
+        if ( (p != nullptr) && (p->getColor()!=color) )
+        {
+            game->setSquare(this,xp,yp);
+            if (!(k->isInCheck()))
+                exists = possibilities[xp][yp] = true;
+            game->setSquare(this,xi,yi);
+            game->setSquare(p,xp,yp);
+        }
+    }
+    return exists;
 }
 
 void Rook::move(int _x, int _y)

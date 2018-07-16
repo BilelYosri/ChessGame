98cdb3947da4b9b8405074a4b11e73f630962d98
YosriMimouna <yosri.mimouna@ensi-uma.tn>
2018-07-16 23:23:14 +0100

    Implemented generatePossibilities() in queen.cpp

--- Chess/queen.cpp
+++ Chess/queen.cpp
@@ -1,5 +1,6 @@
 #include "queen.h"
 #include "game.h"
+#include <cstring>
 
 Queen::Queen(Game* _game, Color _color, int _x, int _y):Piece(_game, _color, _x, _y)
 {
@@ -8,8 +9,46 @@ Queen::Queen(Game* _game, Color _color, int _x, int _y):Piece(_game, _color, _x,
 
 bool Queen::generatePossibilities()
 {
-
+    memset(possibilities,false,sizeof (possibilities));
+    int xi = x;
+    int yi = y;
+    int xp,yp;
+    bool exists = false;
+    bool canMove;
+    Piece* p;
+    King* k = game->getKing();
+    for(int i=0;i<8;i++)
+    {
+        xp = xi;
+        yp = yi;
+        do
+        {
+            canMove=false;
+            xp = xp + QUEEN_X[i];
+            yp = yp + QUEEN_Y[i];
+            p = game->getSquare(xp,yp);
+            if (p == nullptr && !(game->isOutOfBoundaries(xp,yp)))
+            {
+                canMove=true;
+                game->setSquare(this,xp,yp);
+                if (!(k->isInCheck()))
+                    exists = possibilities[xp][yp] = true;
+                game->setSquare(this,xi,yi);
+            }
+        }
+        while( canMove );
+        if ( (p != nullptr) && (p->getColor()!=color) )
+        {
+            game->setSquare(this,xp,yp);
+            if (!(k->isInCheck()))
+                exists = possibilities[xp][yp] = true;
+            game->setSquare(this,xi,yi);
+            game->setSquare(p,xp,yp);
+        }
+    }
+    return exists;
 }
+
 
 void Queen::move(int _x, int _y)
 {

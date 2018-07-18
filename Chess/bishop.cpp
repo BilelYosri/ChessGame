103d605a33d0046cb3145e5991068d64a7ec1781
YosriMimouna <yosri.mimouna@ensi-uma.tn>
2018-07-18 22:32:30 +0100

    implemented move() method in bishop.cpp

--- Chess/bishop.cpp
+++ Chess/bishop.cpp
@@ -51,6 +51,10 @@ bool Bishop::generatePossibilities()
 
 void Bishop::move(int _x, int _y)
 {
-
+    Piece* p = game->getSquare(_x,_y);
+    if ( p != nullptr )
+            game->capture(_x,_y);
+    game->setSquare(nullptr,x,y);
+    game->setSquare(this,_x,_y);
 }
 
